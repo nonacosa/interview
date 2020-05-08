5a231036d5a7113b4fc81221737b0d9a855eaae3
庄文达 <pkwenda@163.com>
2020-05-08 23:31:00 +0800

    AtomicBoolean 用法
    Signed-off-by: 庄文达 <pkwenda@163.com>

---
+++ src/main/java/AtomicBooleanExample.java
@@ -0,0 +1,48 @@
+import sun.jvmstat.perfdata.monitor.CountedTimerTask;
+import sun.jvmstat.perfdata.monitor.PerfStringConstantMonitor;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author wenda.zhuang
+ * @Date 2020/5/8 23:21
+ * @Description 并发中控制某段代码只执行一次
+ * @E-mail sis.nonacosa@gmail.com
+ */
+public class AtomicBooleanExample {
+
+	private static AtomicBoolean isHappened = new AtomicBoolean(false);
+
+	public static int clientTotal = 10000;
+
+	public static int threadTotal = 50;
+
+	public static void main(String[] args) throws InterruptedException {
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		Semaphore semaphore = new Semaphore(threadTotal);
+		CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+		for (int i = 0 ;i < clientTotal; i++) {
+			executorService.execute(() -> {
+				try {
+					semaphore.acquire();
+					if(isHappened.compareAndSet(false,true)) {
+						System.out.println("只执行一次");
+					}
+					semaphore.release();
+					countDownLatch.countDown();
+
+				} catch (Exception e) {
+
+				}
+			});
+
+		}
+
+		countDownLatch.await();
+		executorService.shutdown();
+	}
+}
