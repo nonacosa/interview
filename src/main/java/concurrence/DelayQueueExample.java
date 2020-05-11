b2a0af61ab85e03b729e9ccdd924d01ebf0f4fff
庄文达 <pkwenda@163.com>
2020-05-11 23:42:19 +0800

    DelayQueue
    Signed-off-by: 庄文达 <pkwenda@163.com>

---
+++ src/main/java/concurrence/DelayQueueExample.java
@@ -0,0 +1,10 @@
+package concurrence;
+
+/**
+ * @author wenda.zhuang
+ * @Date 2020/5/11 23:42
+ * @Description 延时队列 todo
+ * @E-mail sis.nonacosa@gmail.com
+ */
+public class DelayQueueExample {
+}
