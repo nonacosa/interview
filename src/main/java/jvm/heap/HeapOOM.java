bc05523d3bfd8c3ff7a098b80b0d08098252f7ec
庄文达 <pkwenda@163.com>
2021-02-26 00:25:03 +0800

    add
    Signed-off-by: 庄文达 <pkwenda@163.com>

--- src/main/java/jvm/heap/HeapOOM.java
+++ src/main/java/jvm/heap/HeapOOM.java
@@ -1,8 +1,24 @@
-package jvm.heap; /**
+package jvm.heap;
+
+/**
+ * @author wenda.zhuang
  * @Date 2021/1/5 01:26
  * @Description ...
- * @E-mail   sis.nonacosa@gmail.com
- * @author  wenda.zhuang
- */   
+ * @E-mail sis.nonacosa@gmail.com
+ */
 public class HeapOOM {
+	public static void main(String[] args) {
+		int i = 0;
+		String a = "hello";
+		try {
+			while (true) {
+				a = a + a;
+				i++;
+				System.out.println(i);
+			}
+		} catch (Throwable e){
+			System.out.println(e); // java.lang.OutOfMemoryError: Java heap space
+			System.out.println(i); // 27
+		}
+	}
 }
